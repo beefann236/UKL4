f110f568df4e6bb6ac527400faaced5b2df252cd
beefann236 <beefan_pratama_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 13:12:06 +0700

    Add files via upload

---
+++ PenjumlahanMatriks.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class PenjumlahanMatriks {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris matriks: ");
+        int baris = input.nextInt();
+        System.out.print("Masukkan jumlah kolom matriks: ");
+        int kolom = input.nextInt();
+
+        int[][] A = new int[baris][kolom];
+        int[][] B = new int[baris][kolom];
+        int[][] C = new int[baris][kolom];
+
+        System.out.println("\nMasukkan elemen matriks A:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("A[" + i + "][" + j + "] = ");
+                A[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("\nMasukkan elemen matriks B:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("B[" + i + "][" + j + "] = ");
+                B[i][j] = input.nextInt();
+            }
+        }
+
+        // Penjumlahan
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        System.out.println("\nHasil penjumlahan matriks (C = A + B):");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(C[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
